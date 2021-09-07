a9a29e3ccb612badd0382387187f1c06fa76225a
obed101 <obedamoako92@gmail.com>
2021-09-07 03:56:31 -0700

    working for correct outpuy

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/env python3
 for i in range(97, 123):
-    """print alphabets in lowercase. the
-    above are the ascii code for 'a' and 'z' respectively
-    """
     print("{}".format(chr(i)), end="")
