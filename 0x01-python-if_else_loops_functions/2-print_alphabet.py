cdf2eb14cbc20b7e5a40db3a8b8a7fea376b89e4
obed101 <obedamoako92@gmail.com>
2021-09-07 03:11:10 -0700

    fixed pep8 error

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -3,4 +3,4 @@ for i in range(97, 122):
     """print alphabets in lowercase. the
     above are the ascii code for 'a' and 'z' respectively
     """
-    print(chr(i),end="")
+    print(chr(i), end="")
