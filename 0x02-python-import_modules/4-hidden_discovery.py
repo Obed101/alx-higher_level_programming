8bbc90206785f96327d41c2066d2e8e93771ffa9
obed101 <obedamoako92@gmail.com>
2021-09-09 14:21:19 -0700

    Redo of task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
-for fn in dir(hidden_4):
-    if fn.startswith('__'):
-        continue
-    print("{}".format(fn))
+if __name__ == "__main__":
+    import hidden_4
+    for name in dir(hidden_4):
+        if name.startswith('__'):
+            continue
+        print("{:s}".format(name))
