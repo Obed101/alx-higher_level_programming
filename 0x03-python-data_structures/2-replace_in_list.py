303b3d834d56c3f9032a6589f0c7af66a9766046
Obed101 <obedamoako92@gmail.com>
2021-09-10 08:08:45 -0700

    added a list modifier

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    ind = len(my_list) - 1
+    if idx < 0 or idx > ind:
+        return my_list
+    else:
+        my_list[idx] = element
+        new_list = my_list 
+        return new_list
