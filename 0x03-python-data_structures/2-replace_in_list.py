d90633ce49db89b744e688d3b94157b6d97ddfab
Obed101 <obedamoako92@gmail.com>
2021-09-13 09:15:34 -0700

    created a program that modifies a particular element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,5 @@ def replace_in_list(my_list, idx, element):
     ind = len(my_list) - 1
     if idx < 0 or idx > ind:
         return my_list
-    else:
-        my_list[idx] = element
-        new_list = my_list
-        return new_list
+    my_list[idx] = element
+    return my_list
