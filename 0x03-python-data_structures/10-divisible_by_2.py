54095b9b145fe0dacef74cafc81a0cda0e882540
Obed101 <obedamoako92@gmail.com>
2021-09-14 02:35:42 -0700

    added a mod2 checker

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python#
 def divisible_by_2(my_list=[]):
-    """    new_list = [False] * len(my_list)
+    new_list = [False] * len(my_list)
     for idx, num in enumerate(my_list):
         if num % 2 == 0:
             new_list[idx] = True
     return new_list
-    """
-    return True if my_list % 2 = 0 else False
