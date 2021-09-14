f2061d28e062c0afafcc94676cc880464e366a5e
Obed101 <obedamoako92@gmail.com>
2021-09-14 02:10:08 -0700

    added a matrix printer

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in my_string:
+    for row in matrix:
         for val in row:
-        print(val, end=" ")
-
+        print({:d}.format(val), end="")
+        if val != row[-1]:
+            print(" ", end='')
+    print()
