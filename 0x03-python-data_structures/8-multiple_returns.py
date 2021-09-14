38dbc506516f35b117be185c28dd7cb7cc171f2d
Obed101 <obedamoako92@gmail.com>
2021-09-14 08:52:50 -0700

    changed to comprehension for ease

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) > 0:
-        stat = (len(sentence), sentence[0])
-    else:
-        sentence[0] = None
-    return stat
+        return (len(sentence), sentence[0] if len(sentence) > 0 else None)
