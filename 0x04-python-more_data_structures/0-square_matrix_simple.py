a37d568fda6b7076ec69ac229746db2f1b4fbdb7
Obed101 <obedamoako92@gmail.com>
2021-09-15 12:01:42 -0700

    Added a function that calculates matrix squares

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix != None:
+        new_matrix = [[n * n for n in a] for a in matrix]
+        return new_matrix
+    else:
+        return
