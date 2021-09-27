47f674555a4b6b4efab3bfa83fab932b5b2f69f9
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-09-27 20:04:27 +0000

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{d}".format(value), end='')
-    except typeError:
-            print("\n")
-    return True if value else False
+        print("{:d}".format(value))
+        return True
+    except:
+        return False
