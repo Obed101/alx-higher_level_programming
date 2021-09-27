314ac82244944a86f57d4eed52f0cb02c9900125
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-09-27 14:45:38 +0100

    Added more ability to except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,5 +3,5 @@ def safe_print_list(my_list=[], x=0):
     try:
             print("{}".format(my_list[x]), end='')
             x + 1
-    except IndexError:
+    except (IndexError, TypeError):
         print("\n")
