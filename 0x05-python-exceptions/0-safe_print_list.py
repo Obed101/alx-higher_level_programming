5bed5bfb5fec8b6f7b8fcd707c69a1f1db1dbe2d
Obed101 <obedamoako92@gmail.com>
2021-09-27 06:29:49 -0700

    Used try and except to print the content of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-  
+    try:
+        for x in my_list:
+            print("{}".format(my_list[x]))
+    except IndexError:
+        print("\n")
