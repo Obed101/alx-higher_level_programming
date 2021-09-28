0cd6a10ce409f786a1a524b5fc3644f59a7ea4ea
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-09-28 16:40:38 +0100

    Pycodestyle fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
+""" Tthis module defines a class named as square """
 class Square:
-
+""" name of class """
     def __int__(self, size):
+        """ initializing the square """
         self.__size = size
