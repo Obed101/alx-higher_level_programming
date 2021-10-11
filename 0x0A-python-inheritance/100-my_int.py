b7a75244ad972864e33ae8bc486b850307f5c5c5
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-10-11 17:57:20 +0100

    Reupdated reversion

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,10 +5,20 @@
 class MyInt(int):
     """This class creates a personalized int"""
 
+    def __init__(self, num):
+        """initializes the class"""
+        self.num = num
+
     def __eq__(self, other):
         """handles the == operator"""
-        return True if self != other else False
-    
+        if self.num != other:
+            return True
+        else:
+            return False
+
     def __ne__(self, other):
-        """reverts the != operator"""
-        return True if self == other else False
+        if self.num < 0:
+            return True if self.num != other else False
+        if self.num == other:
+            return True
+        return False
