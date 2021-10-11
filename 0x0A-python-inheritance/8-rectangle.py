62343b9c0f5a77389e243325849e38f2c85d8c2c
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-10-11 16:12:41 +0100

    Changed integer validation

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,9 +7,8 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """Defines a rectangle"""
     def __init__(self, width, height):
+        """instanciation initialiser"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = int(width)
         self.__height = int(height)
-        if type(height) != int:
-            raise TypeError("height must be an integer")
-        if type(width) != int:
-            raise TypeError("width must be an integer")
