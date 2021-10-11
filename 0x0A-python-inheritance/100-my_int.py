8ed8413ee3c4fc7ffa07c5915136e12c198a858d
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-10-11 17:17:07 +0100

    Added a personalized int class

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module inverts comparison operators"""
+
+
+class MyInt(int):
+    """This class creates a personalized int"""
+    def __init__(self, num):
+        """initializes the class"""
+        self.num = num
+        super().__init__()
+
+    def __eq__(self, other):
+        """handles the == operator"""
+        if self.num != other:
+            return True
+        else:
+            return False
+        # return not other
+    
+    def __ne__(self, other):
+        if self.num < 0:
+            return True if self.num != other else False
+        if self.num == other:
+            return True
+        return False
