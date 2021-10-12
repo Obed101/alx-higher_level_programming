899bd6378c5c24401bacfa8e05aea49406a3df1a
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-10-12 22:12:21 +0100

    remade file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,15 +2,20 @@
 """Module for file input/output"""
 
 import sys
+import os.path
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+write_file = __import__('1-write_file').write_file
 
-all_args = list(sys.argv[1:])
+name_file = "add_item.json"
 
-try:
-    initial = load_from_json_file('add_item.json')
-except Exception:
-    initial = []
+if not os.path.exists(name_file):
+    write_file(name_file, "[]")
 
-initial.extend(all_args)
-save_to_json_file(initial, 'add_item.json')
+new_list = load_from_json_file(name_file)
+argc = len(sys.argv)
+for i in range(1, argc):
+    new_list += [sys.argv[i]]
+
+save_to_json_file(new_list, name_file)
