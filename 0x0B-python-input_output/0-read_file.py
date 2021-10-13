9cbf0fda22ca5ce0e82facbeb5fe20e12c421dec
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-10-13 09:09:07 +0100

    with

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,5 @@
 
 def read_file(filename=""):
     """reads a txt file and prints it to stdout"""
-    file = open(filename, "r")
-    print(file.read())
-    file.close()
+    with open(filename, "r") as file:
+    	print(file.read())
