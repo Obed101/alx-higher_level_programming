c277c2caa5f7757c5899d399889a4f9a1e58d629
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-10-13 19:22:08 +0100

    fixing

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Input/output module"""
+"""Defines a file-appending function."""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """Appends a string to the end of a UTF8 text file.
     Args:
         filename (str): The name of the file to append to.
@@ -10,5 +10,5 @@ def write_file(filename="", text=""):
     Returns:
         The number of characters appended.
     """
-    with open(filename, "a", encoding='utf-8') as file:
-        return file.write(text)
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
