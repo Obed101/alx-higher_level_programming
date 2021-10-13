d89dae9c8d23e55e24af5a9938bcd1cd63891d54
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-10-13 21:16:53 +0100

    remade the whole thing (making the code look attractive)

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""Find and insert"""
+'''task 13 module'''
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """inserts some text after finding a particular text"""
-    with open(filename, 'r') as f:
+    '''inserts a line of text to a file
+    after each line containing a specific string
+    '''
+    with open(filename, mode='r', encoding='utf-8') as f:
         lines = f.readlines()
 
-    with open(filename, 'r') as f:
+    with open(filename, mode='w', encoding='utf-8') as f:
         new_lines = []
         for i in range(len(lines)):
             new_lines.append(lines[i])
