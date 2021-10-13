affa50c8bb25ee5404ec04c1a74d04e9f01ea22e
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-10-13 16:28:55 +0100

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,5 +5,4 @@
 def write_file(filename="", text=""):
     """writes to a txt file"""
     with open(filename, "a", encoding='utf-8') as file:
-        file.write(text)
-    return len(text)
+        return file.write(text)
