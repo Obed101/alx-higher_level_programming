adbb795d2f07a3eb1bea27f504ee1cd015c41768
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-10-13 07:45:07 +0100

    whitespace error

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -14,11 +14,11 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-        
+
     def __str__(self):
         """prints a string of how the area is calculated"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
-    
+
     def area(self):
         """returns area of rectangle"""
         return self.__width * self.__height
