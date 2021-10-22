acf6930ed8188d380a8c577aa96927035552b7ef
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-10-22 22:24:26 +0100

    Files with square have correct features now

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,54 +1,60 @@
-#!/usr/bin/python3
-"""this module prints a square"""
+"""
+This module defines the Square class
+"""
 
-
-from rectangle import Rectangle
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """a square that inherits from rectangle"""
+    """A Square inherits from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialization of the instance"""
+        """initializing the attributes"""
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     @property
     def size(self):
-        """retrieves the size => width attribute"""
-        return self.__width
+        """getter of size"""
+        return self.width
 
     @size.setter
     def size(self, value):
-        """sets the new value to the size attribute"""
-        self.__width = value
-        self.__height = value
-
-    def update(self, *args, **kwargs):
-        """Updates the instance attributes in particular order"""
-        i = 0
-        attributes = ['id', 'size', 'x', 'y']
-        if len(args) > 0:
-            for attr in attributes:
-                if i > len(args) - 1:
-                    break
-                setattr(self, attr, args[i])
-                i += 1
-        else:
-            for key, value in kwargs.items():
-                if key not in attributes:
-                    continue
-                setattr(self, key, value)
-
-    def to_dictionary(self):
-        """returns the dict form of the Square instance"""
-        return {
-                'id': self.id,
-                'size': self.size,
-                'x': self.__x,
-                'y': self.__y
-                }
+        """setter of size"""
+        self.width = value
+        self.height = value
 
     def __str__(self):
-        """returns the string representation of the instance"""
-        fh = '[Square] ({:d}) {:d}/{:d}'.format(self.id, self.__x, self.__y)
-        sh = ' - {:d}'.format(self.__width)
-        return fh + sh
+        """string representation of the square"""
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,
+                                                         self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        """updates the attributes"""
+        if len(args):
+            for i, a in enumerate(args):
+                if i == 0:
+                    self.id = a
+                elif i == 1:
+                    self.size = a
+                elif i == 2:
+                    self.x = a
+                elif i == 3:
+                    self.y = a
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        """now in dict form"""
+        d = {}
+        d["id"] = self.id
+        d["size"] = self.size
+        d["x"] = self.x
+        d["y"] = self.y
+        return d
