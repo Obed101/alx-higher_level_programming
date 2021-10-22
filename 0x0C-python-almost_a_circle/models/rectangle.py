c7d21bf93800c717c512babe0603eb2c260aa3cb
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-10-22 21:34:14 +0100

    removed the python3.9 code

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,8 +85,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """prints the attributes in a string format"""
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y}"\
-               " - {self.__width}/{self.__height}"
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
 
     def display(self):
         """prints the rectangle instance with the # character"""
