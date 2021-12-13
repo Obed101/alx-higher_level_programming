ef449871122b05a4efac9ff5b2e2f21986a6d6b0
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-13 20:13:11 +0000

    loop throuh

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,7 @@
 
 const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let iterator = 0;
-while (iterator <= myArray.length){
+while (iterator <= myArray.length) {
   iterator++;
+  console.log(iterator);
 }
-console.log(iterator);
