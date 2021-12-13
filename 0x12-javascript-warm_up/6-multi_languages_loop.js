91ebed78163c2a7b96ad39e46dc36c1a357971a7
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-13 21:23:16 +0000

    fixed typo

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,5 +2,5 @@
 const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let loop;
 for (loop = 0; loop < myArray.length; loop++) {
-  console.log(myArray.loop);
+  console.log(myArray[loop]);
 }
