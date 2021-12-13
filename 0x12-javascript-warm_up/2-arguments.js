dc1a1de2ff6cb65d30ca2cae8c3e3899655fa252
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-13 18:34:05 +0000

    made them multi line

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 
 const process = require('process')
-let args = process.argv
+let count = process.argv
 
-if (args.lenght === 0) console.log("No argument")
-if (args.lenght === 1) console.log("Argument found")
-else {
-  console.log("Arguments found")
+if (count === 1) {
+  console.log('No argument');
+} else if (count === 2) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
