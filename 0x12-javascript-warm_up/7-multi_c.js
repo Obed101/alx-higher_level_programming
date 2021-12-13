79cfcb8367fc6b38633b72e91760015d124339fd
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-13 20:02:03 +0000

    increment

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -6,6 +6,7 @@ let argument = Number(process.argv[1]);
 if (argument !== NaN){
   while (iterator <= argument){
     console.log('C is fun');
+    iterator++;
   }
 } else {
     console.log('Missing number of occurrences');
