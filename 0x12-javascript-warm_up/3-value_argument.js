7dff86c3f74559eee2da22ec717f9b05340b43d5
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-13 20:57:35 +0000

    reframe

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 
-const process = require('process')
-let args = process.argv
-
-if (args[2] === 0) console.log("No argument")
-
-else {
-  console.log(args[2])
+const args = process.argv[2];
+if (args === 0) {
+  console.log('No argument');
+} else {
+  console.log(args);
 }
