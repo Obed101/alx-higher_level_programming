0e1285c02d3af23539c69fc1704ab1fdd0dee412
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-13 20:38:42 +0000

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let iterator = 0;
-while (iterator < myArray.length) {
-  iterator++;
-  console.log(myArray.iterator);
+let loop;
+for (loop = 0; loop < myArray.length; loop++) {
+  console.log(myArray.loop);
 }
