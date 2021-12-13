ad6c19af53cd7fbef9f739bdb5077eedf21f0082
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-13 19:57:46 +0000

    Modified import

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-import { argv } from 'process';
+const process = require('process');
+
 let iterator = 1;
-let argument = Number(argv[1]);
+let argument = Number(process.argv[1]);
 if (argument !== NaN){
   while (iterator <= argument){
     console.log('C is fun');
