74b4b0ef4aaefd9a59cc060eef0b8e0fda5d093c
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-13 19:23:36 +0000

    Added all languages loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let iterator = 0;
+while (iterator <= myArray.length){
+  iterator++;
+}
+console.log(iterator);
