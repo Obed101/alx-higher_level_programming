0b9d2d4562758f02572b6e15f676be01a7c76446
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-14 14:22:08 +0000

    added task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const Square1 = require('./5square')
+class Square extends Square1 {
+  constructor (size) {
+    super(size);
+
+    charPrint (c) {
+      if (c) {
+      for (let i = 0; i < this.size; i++) {
+        console.log(c.repeat(this.size));
+      }
+      } else {
+        for (let i = 0; i < this.size; i++) {
+            console.log('X'.repeat(this.size));
+        }
+      }
+    }
+  }
+}
