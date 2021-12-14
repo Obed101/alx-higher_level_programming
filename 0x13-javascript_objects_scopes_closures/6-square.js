9eef37e2e56a193cd36498f5db4db1f51f686138
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-14 17:56:56 +0000

    making little changes

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,14 +3,12 @@ const Square1 = require('./5-square');
 
 class Square extends Square1 {
   charPrint (c) {
-    if (c) {
-      for (let i = 0; i < this.size; i++) {
-        console.log(c.repeat(this.size));
-      }
-    } else {
-      for (let i = 0; i < this.size; i++) {
-        console.log('X'.repeat(this.size));
-      }
+    let cp = c;
+    if (!c) {
+      cp = 'X';
+    }
+    for (let i = 0; i < this.size; i++) {
+      console.log(cp.repeat(this.size));
     }
   }
 }
