b783b017ed68e6b130802930cc69e8fc61c03ae3
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-14 12:28:03 +0000

    included import

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     this.width = w;
     this.height = h;
     if (!w || !h) {
@@ -10,3 +10,4 @@ class Rectangle {
     }
   }
 }
+modules.exports = Rectangle;
