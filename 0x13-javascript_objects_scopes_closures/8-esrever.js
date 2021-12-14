6baa53f8296b08a1b38bffdd2e14b863376edef8
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-14 22:33:04 +0000

    push not working: changed

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
+#!/usr/bin/node
 exports.esrever = function (list) {
-    if (list) {
-        const rev = [];
-        const size = list.lenght - 1;
-
-        for (let i = size; i >= 0; i--) {
-            rev.push(list[i]);
-        }
-        return rev.join('');
-    }
-}
+  const listed = [];
+  list.forEach(elem => {
+    listed.unshift(elem);
+  });
+  return listed;
+};
