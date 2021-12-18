367f708ece61d277e003156d5c0012775d3a7e88
Obed101 <obedamoako92@gmail.com>
2021-12-18 09:10:19 -0800

    SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This module uses sqlalchemy to print table
+"""
+import sys
+import MySQLdb as sql
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
+
+
+def display_first():
+    """This method connects to a database using sqlalchemy"""
+    host = "localhost"
+    port = 3306
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
+        username, passwd, host, port, database),
+                           pool_pre_ping=True, poolclass=NullPool)
+    Session = sessionmaker(bind=engine)
+    new_session = Session()
+    states = new_session.query(State).order_by(State.id).first()
+    new_session.close()
+    if states:
+        first1 = states.id + ": " + states.name
+    else:
+        first1 = "Nothing"
+    print(first1)
+
+if __name__ == '__main__':
+    display_first()
