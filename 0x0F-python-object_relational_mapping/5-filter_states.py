7bb538812dcdaed7a41c5265c96b3f4e28e7ab5a
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-18 12:48:54 +0000

    Added a function that searches for a state

---
+++ 0x0F-python-object_relational_mapping/5-filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module uses the MySQLdb module to execute SQL commands.
+(Best way to access database in python)
+It does operations on @cities and @states Table
+"""
+import sys
+import MySQLdb as sql
+
+
+def filter_cities():
+    """This method Uses mySQL queries to filter cities"""
+    host = "localhost"
+    port = 3306
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    connection = sql.connect(host=host, user=user,
+                             passwd=passwd, db=sys.argv[3], port=port)
+    cur = connection.cursor()
+    state_name = sys.argv[4]
+    sql_cmd1 = 'SELECT states.name FROM cities LEFT '
+    sql_cmd2 = 'JOIN states ON cities.state_id = states.id ORDER BY cities.id;'
+    cur.execute(sql_cmd1 + sql_cmd2)
+    result = cur.fetchall()
+
+    if result:
+        for states in result:
+            print(states)
+    cur.close()
+    connection.close()
+
+
+if __name__ == '__main__':
+    filter_cities()
