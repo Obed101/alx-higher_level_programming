01db8aa2b74ca38217d6e474692a6ef55026239d
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-27 12:21:31 +0000

    Aliasing

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- This script filters TABLES using JOIN
 SELECT cities.id, cities.name, states.name FROM cities c
 INNER JOIN states s
-ON cities.state_id = states.id
+ON c.state_id = s.id
 ORDER BY cities.id;
