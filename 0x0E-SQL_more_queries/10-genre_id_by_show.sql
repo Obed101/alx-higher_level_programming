057da3d7b9ef3894f974a16f0ed44cde085aea5b
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-27 12:37:21 +0000

    genre id by show :)

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- This script does it all
 SELECT ts.title, tg.genre_id FROM tv_shows ts
 INNER JOIN tv_show_genres tg
-ON tg.show_id = tg.genre_id
+ON ts.show_id = tg.genre_id
 ORDER BY ts.title, tg.genre_id ASC;
