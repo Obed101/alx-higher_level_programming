041db5d0eef424b500dcd5949e9d98bb2b5f793a
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-27 11:06:23 +0000

    uncomment for right output

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- This script filters TABLES using JOIN
 SELECT cities.id, cities.name, states.name FROM cities c
 INNER JOIN states s
--- ON cities.state_id = states.id
+ON cities.state_id = states.id
 ORDER BY cities.id;
