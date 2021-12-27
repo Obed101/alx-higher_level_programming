78907ceb0af23717d1bcc88d18aec210c3d424cf
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-27 12:47:57 +0000

    left join done

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- This script does it all
 SELECT ts.title, tg.genre_id FROM tv_shows ts
 LEFT JOIN tv_show_genres tg
-ON tg.title = tg.genre_id OR tg.show_id = NULL
+ON tg.show_id = ts.id OR tg.show_id = NULL
 ORDER BY ts.title, tg.genre_id ASC;
