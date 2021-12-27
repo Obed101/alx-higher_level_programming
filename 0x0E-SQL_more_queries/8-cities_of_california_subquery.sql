7e17defdeb3b12f1df797f40709b8d3345a2d3d5
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-27 11:00:09 +0000

    cities table completed

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- This script filters TABLES
 SELECT id, name FROM cities c
-WHERE c.state_id = (SELECT id FROM states WHERE name = 'Califonia')
-ORDER BY id;
+WHERE c.state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY id ASC;
