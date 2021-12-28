86507fe98eda54ebc51534d439aa68bd261e12f8
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-28 21:11:50 +0000

    genres filter

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,7 +2,7 @@
 SELECT ts.title FROM tv_shows ts
 INNER JOIN tv_show_genres tg
 ON ts.id = tg.show_id
-INNER JOIN tv_genre tvg
+INNER JOIN tv_genres tvg
 ON tvg.id = tg.genre_id
 WHERE tvg.title = "Comedy"
 ORDER BY ts.title ASC;
