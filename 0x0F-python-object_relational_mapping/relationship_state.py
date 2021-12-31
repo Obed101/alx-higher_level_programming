4e06eb056876d20fb90aa0f7446565705a32caec
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-31 21:42:58 +0000

    Best way documentation

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" This module creates a relationship with the cities
+child for the State
+"""
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
