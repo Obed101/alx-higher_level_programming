80470eba695a3c318947bf8642ef0d44e96077be
Obed101 <obedamoako92@gmail.com>
2022-01-13 19:02:51 +0000

    error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
 """ This script requests error code and print it"""
+import sys
+import urllib.error
+import urllib.request
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+
+    req = urllib.request.Request(url)
+    try:
+    with urllib.request.urlopen(request) as web:
+        print(web.read()).decode("ascii")
+
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
