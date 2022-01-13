79112684c3e9c37b435305a35e79228cc1c822f3
Obed101 <obedamoako92@gmail.com>
2022-01-13 12:36:44 +0000

    status_code

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" This module reads URL from server"""
+import urllib.request as lib
+
+with lib.urlopen('https://intranet.hbtn.io/status') as ericob:
+    view = ericob.read()
+print("Body response:")
+print("\t - type: {}".format(type(view)))
+print("\t - {}".format(view))
+print("\t - utf8 content: {}".format(view.decode()))
