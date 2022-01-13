b3817f6c6b6aa7e30f0ced2056af6f4a71afc7ae
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2022-01-13 13:25:28 +0000

    modified to respond to command line non import

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,10 @@
 """ This module reads URL from server"""
 import urllib.request as lib
 
-with lib.urlopen('https://intranet.hbtn.io/status') as ericob:
-    view = ericob.read()
+if __name__ == '__main__':
+    with lib.urlopen('https://intranet.hbtn.io/status') as response:
+        view = response.read()
 print("Body response:")
-print("\t- type: {}".format(type(view)))
-print("\t- content {}".format(view))
-print("\t- utf8 content: {}".format(view.decode()))
+print("    - type: {}".format(type(view)))
+print("    - content: {}".format(view))
+print("    - utf8 content: {}".format(view.decode()))
