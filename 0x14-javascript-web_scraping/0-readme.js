86e76a3a2f96018800225a12e9c565fd55da96f4
Obed101 <obedamoako92@gmail.com>
2022-02-01 12:32:16 +0000

    javascript readfile

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// This file is reading the content of a txt file
+const fs = require('fs')
+const myArgs = process.argv.slice(2);
+fs.readFile(myArgs[1], 'utf-8', (err, data) => {
+	if (err) throw err;
+	console.log(data);
+})
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,1 @@
+# Handling Url Request Using Javascript
