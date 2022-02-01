582595136a657060dff2e3899934df486c7e5250
Obed101 <obedamoako92@gmail.com>
2022-02-01 16:38:05 +0000

    starwars

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// This file prints data from a Star Wars API
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/{}'.format(id);
+const request = require('requests');
+
+request(url, (error, response) => {
+  if (error) console.log(error);
+
+	if (response.keys('episode_id') === id) console.log(response.keys('title'));
+});
