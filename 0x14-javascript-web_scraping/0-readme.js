b899fcdec455add60cb46540886a66c723da094e
Obed101 <obedamoako92@gmail.com>
2022-02-01 14:12:42 +0000

    slice removed

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // This file is reading the content of a txt file
 const fs = require('fs');
-const myArgs = process.argv.slice(2);
+const myArgs = process.argv;
 fs.readFile(myArgs[1], 'utf-8', (err, data) => {
 	if (err) throw err;
 	console.log(data);
