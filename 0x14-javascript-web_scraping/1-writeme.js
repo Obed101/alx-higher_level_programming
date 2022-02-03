a8d54cf1e9f0420fec71f4b05ac734426398d5c8
Obed101 <obedamoako92@gmail.com>
2022-02-03 10:19:55 +0000

    semistandard fixed

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,5 +4,5 @@ const arg = process.argv;
 const fs = require('fs');
 
 fs.writeFile(arg[2], arg[3], 'utf-8', (err) => {
-if (err) throw err;
+  if (err) throw err;
 });
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -7,5 +7,5 @@ const request = require('requests');
 request(url, (error, response) => {
   if (error) console.log(error);
 
-	if (response.keys('episode_id') === id) console.log(response.keys('title'));
+  if (response.keys('episode_id') === id) console.log(response.keys('title'));
 });
